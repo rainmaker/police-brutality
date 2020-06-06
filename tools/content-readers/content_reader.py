02b2d354d81b042f711b80c3e84317da00145c74
Evan Marcey <emarcey@cbinsights.com>
2020-06-06 16:55:21 -0400

    Started work on reddit content reader. Added some stubs for mongo funcs

--- tools/content-readers/content_reader.py
+++ tools/content-readers/content_reader.py
@@ -1,24 +1,18 @@
 import os
 import time
 
+from common.config import JOB_SLEEP_TIME_SECONDS, READER_MODE
 from reddit_rss.feed_reader import run_reddit_rss_feed
 
 
 if __name__ == "__main__":
-    READER_MODE = os.getenv("READER_MODE")
-    if not READER_MODE:
-        raise ValueError(f"READER_MODE not set.")
-
-    JOB_SLEEP_TIME_SECONDS = int(os.getenv("JOB_SLEEP_TIME_SECONDS"))
-    if not JOB_SLEEP_TIME_SECONDS:
-        JOB_SLEEP_TIME_SECONDS = 30
-
     try:
         while True:
             if READER_MODE == "reddit":
                 run_reddit_rss_feed()
             else:
-                raise ValueError(f"READER_MODE {READER_MODE} not supperted")
+                raise ValueError(f"READER_MODE {READER_MODE} not supported")
+            print(f"Job complete. Sleeping for {JOB_SLEEP_TIME_SECONDS} seconds.")
             time.sleep(JOB_SLEEP_TIME_SECONDS)
     except KeyboardInterrupt:
         print("KeyboardInterrupt called. Exiting.")
