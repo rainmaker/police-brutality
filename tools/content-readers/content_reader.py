f25499061528b32bbcb0a22df45a61f41ac7869a
Evan Marcey <emarcey@cbinsights.com>
2020-06-06 12:59:49 -0400

    Adding sleep time

--- tools/content-readers/content_reader.py
+++ tools/content-readers/content_reader.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from reddit_rss.feed_reader import run_reddit_rss_feed
 
@@ -8,5 +9,16 @@ if __name__ == "__main__":
     if not READER_MODE:
         raise ValueError(f"READER_MODE not set.")
 
-    if READER_MODE == "reddit":
-        run_reddit_rss_feed()
+    JOB_SLEEP_TIME_SECONDS = int(os.getenv("JOB_SLEEP_TIME_SECONDS"))
+    if not JOB_SLEEP_TIME_SECONDS:
+        JOB_SLEEP_TIME_SECONDS = 30
+
+    try:
+        while True:
+            if READER_MODE == "reddit":
+                run_reddit_rss_feed()
+            else:
+                raise ValueError(f"READER_MODE {READER_MODE} not supperted")
+            time.sleep(JOB_SLEEP_TIME_SECONDS)
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt called. Exiting.")
